dd93e1e02afd3f29ad7b13eb6ca526f314d22b3b
Bojan Čoka <coka@fastmail.com>
2020-02-08 13:15:32 +0100

    Define initial tab bar styles

--- frontend/src/screens/LoggedInApplication.tsx
+++ frontend/src/screens/LoggedInApplication.tsx
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import About from './About'
 import Pickups from './Pickups'
 
@@ -13,11 +13,88 @@ const ProfileScreen = () => (
 )
 
 const LoggedInApplication = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Profile" component={ProfileScreen} />
-    <Tab.Screen name="Pickups" component={Pickups} />
-    <Tab.Screen name="Community" component={Pickups} />
-    <Tab.Screen name="About" component={About} />
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: '#8dc63f',
+      inactiveBackgroundColor: '#8dc63f',
+    }}
+  >
+    <Tab.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        tabBarLabel: ({ focused }) => {
+          const opacity = focused ? 1 : 0.6
+          return (
+            <View style={[styles.tabLabel, { opacity }]}>
+              <Image
+                style={styles.tabLabelIcon}
+                resizeMode="contain"
+                source={require('../../assets/user.png')}
+              />
+              <Text style={styles.tabLabelText}>PROFIL</Text>
+            </View>
+          )
+        },
+      }}
+    />
+    <Tab.Screen
+      name="Pickups"
+      component={Pickups}
+      options={{
+        tabBarLabel: ({ focused }) => {
+          const opacity = focused ? 1 : 0.6
+          return (
+            <View style={[styles.tabLabel, { opacity }]}>
+              <Image
+                style={styles.tabLabelIcon}
+                resizeMode="contain"
+                source={require('../../assets/pickup.png')}
+              />
+              <Text style={styles.tabLabelText}>PREUZIMANJA</Text>
+            </View>
+          )
+        },
+      }}
+    />
+    <Tab.Screen
+      name="Community"
+      component={Pickups}
+      options={{
+        tabBarLabel: ({ focused }) => {
+          const opacity = focused ? 1 : 0.6
+          return (
+            <View style={[styles.tabLabel, { opacity }]}>
+              <Image
+                style={styles.tabLabelIcon}
+                resizeMode="contain"
+                source={require('../../assets/community.png')}
+              />
+              <Text style={styles.tabLabelText}>ZAJEDNICA</Text>
+            </View>
+          )
+        },
+      }}
+    />
+    <Tab.Screen
+      name="About"
+      component={About}
+      options={{
+        tabBarLabel: ({ focused }) => {
+          const opacity = focused ? 1 : 0.6
+          return (
+            <View style={[styles.tabLabel, { opacity }]}>
+              <Image
+                style={styles.tabLabelIcon}
+                resizeMode="contain"
+                source={require('../../assets/shield.png')}
+              />
+              <Text style={styles.tabLabelText}>O APLIKACIJI</Text>
+            </View>
+          )
+        },
+      }}
+    />
   </Tab.Navigator>
 )
 
@@ -26,6 +103,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
+  },
+  tabLabel: {
+    alignItems: 'center',
+  },
+  tabLabelIcon: {
+    height: 24,
+    marginBottom: 4,
+  },
+  tabLabelText: {
+    color: '#ffffff',
+    fontFamily: 'oswald',
+    fontSize: 10,
+    letterSpacing: 0.15,
+    lineHeight: 12,
   },
 })
 
